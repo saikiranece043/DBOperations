9f5676cdf3d3c71f356aa6bc0edf67fd8e1634d9
saikiranpotti <saikiranece043@gmail.com>
2020-06-25 17:15:42 -0400

    comments are added to the functions in groupby

--- src/group.py
+++ src/group.py
@@ -1,5 +1,7 @@
-import argparse, sys, re, subprocess
+import argparse, sys, re
 import copy
+from prettytable import PrettyTable
+
 
 def user_interface():
 	resultlist = []
@@ -19,6 +21,103 @@ def user_interface():
 	resultlist.append(args.function)
 	return resultlist
 
+
+
+def column_offset_validation(arguments):
+	"""
+    This function is to validate the column offset and return user friendly errors
+    It also returns the line against which it performs the column reference validation
+    :param : arguments - args provided by the user to the program
+    :return: returns the first line of the input stream (file or stdin)
+    """
+
+
+	aggattrlist = []
+
+
+	for func in arguments[5].split(','):
+		aggattrlist.append(re.search(r'\((.*?)\)', func).group(1))
+
+	groupingattributes = arguments[0].split(',')
+
+	inputfile = arguments[1]
+	header = inputfile.readline()
+	splitter = arguments[4]
+	attributesCount = len(header.split(splitter))
+	#operands = arguments[0].split(',')
+	operands = aggattrlist
+	hasheader = arguments[3]
+
+	if hasheader:
+		for operand in operands:
+
+			# if you are here the column offset can be a integer or string
+			if operand[1:].isdecimal():
+				data_error_handler(operand, attributesCount, arguments , type= "aggregation")
+			else:
+				# This block of code is executed for float or string
+				if operand[1:] not in header:
+					print(f'aggregate column reference {operand} entered is incorrect')
+					free_resources(arguments)
+					sys.exit(-1)
+
+		for attr in groupingattributes:
+			if attr.isdecimal():
+				data_error_handler("#"+ str(attr), attributesCount, arguments, type= "grouping")
+			else:
+				# This block of code is executed for float or string
+				if attr not in header:
+					print(f'grouping column reference {operand} entered is incorrect')
+					free_resources(arguments)
+					sys.exit(-1)
+
+
+	else:
+		# no header so setting the file pointer back to first line
+		# if inputtype != None: (while going back is an option in files not for stdin)
+		#    inputfile.seek(0)
+		for operand in operands:
+			if operand[1:].isdecimal():
+				data_error_handler(operand, attributesCount, arguments , type = "aggregation")
+			else:
+				print(f'grouping column reference {operand} cannot be a string, perhaps you forgot to pass "-h" arg')
+				free_resources(arguments)
+				sys.exit(-1)
+
+		#print(groupingattributes)
+		for attr in groupingattributes:
+			if attr.isdecimal():
+				data_error_handler("#"+ str(attr), attributesCount, arguments , type = "grouping")
+			else:
+				# This block of code is executed for float or string
+				print(f'aggregate column reference {operand} cannot be a string, perhaps you forgot to pass "-h" arg')
+				free_resources(arguments)
+				sys.exit(-1)
+
+	return header
+
+
+def data_error_handler(data, attributesCount, arguments , type):
+	"""
+    Function performs validation of the input data
+    Checks if the data is string or integer
+    If the data is integer it further checks if it's contained within a range
+    :param data: data to validate for errors
+    :param attributesCount: number of columns in the input
+    :param arguments: args provided by the user to the program
+    :return: void
+    """
+	# if you are here that means the column offset should always be an integer
+
+	if not data[1:].isdecimal():
+		print(f'The column offset {data}  in {type} should be an integer')
+		free_resources(arguments)
+		sys.exit(-1)
+	# the column offset should be between 0...(attributesCount - 1)
+	if int(data[1:]) not in range(1, attributesCount+1):
+		print(f'The column offset {data} in {type} should be in the range (1, {attributesCount - 1}) ')
+		free_resources(arguments)
+		sys.exit(-1)
 
 def free_resources(arguments):
 	"""
@@ -121,80 +220,13 @@ def aggregate_func(aggfunc, line, args, attr, init):
 		sys.exit(-1)
 
 
-def sort_input(args):
-	attributes, input, output, hasheader, split, aggfunc = args
-	cmd = [ "sort", "-k", attributes , "-t", split ]
-	proc = subprocess.Popen(cmd,stdin=input, stdout=subprocess.PIPE, stderr= subprocess.PIPE)
-	o, e = proc.communicate()
-	#print('Output: ' + o.decode('ascii'))
-
-	with open("sortedfile","w") as f:
-		f.write(o.decode())
-
-
-
-def group_by(args):
-
-	attributes, input, output, hasheader, split, aggfunc = args
-	input = open("sortedfile")
-	aggfunclists = aggfunc.split(',')
-	attrlist = attributes.split(',')
-
-	aggattrlist = []
-	aggfunclist = []
-
-	for func in aggfunclists:
-		aggattrlist.append(re.search(r'\((.*?)\)', func).group(1))
-		aggfunclist.append(func.split('(')[0])
-
-
-
-	# an array to hold the aggregation values
-	init = []
-	init = reset_init(aggfunclist,args,init)
-
-	# initiating variables
-	firstIteration = True
-	currentgroupstring = ""
-	previousgroupstring = ""
-
-	# reading line by line and performing aggregations
-	for line in input:
-		datalist = line.split(split)
-		currentgroupstring = "".join([datalist[int(attr)-1] for attr in attrlist])
-
-		# executed only once for the first time
-		if firstIteration:
-			firstIteration = False
-			for index in range(0, len(aggattrlist)):
-				init[index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index], init[index])
-			previousgroupstring =  currentgroupstring
-
-		else:
-			## that means same group so keep incrementing
-			if currentgroupstring == previousgroupstring:
-				for index in range(0, len(aggattrlist)):
-					init[index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index], init[index])
-			## you are here means new group flush the init values to stdout or file, reset the init value
-			else:
-				output.write(previousgroupstring + " ")
-				output.write(" ".join([str(x) for x in init]))
-				output.write('\n')
-				init = []
-				init = reset_init(aggfunclist, args, init)
-				for index in range(0, len(aggattrlist)):
-					init[index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index], init[index])
-				previousgroupstring = currentgroupstring
-
-	output.write(previousgroupstring + " ")
-	output.write(" ".join([str(x) for x in init]))
-	output.write('\n')
 
 
 
 
 
-def group_by_hash(args):
+
+def group_by_hash(args,firstline):
 
 	attributes, input, output, hasheader, split, aggfunc = args
 	aggfunclists = aggfunc.split(',')
@@ -206,44 +238,86 @@ def group_by_hash(args):
 	for func in aggfunclists:
 		aggattrlist.append(re.search(r'\((.*?)\)', func).group(1))
 		aggfunclist.append(func.split('(')[0])
-
-
 
 	# an array to hold the aggregation values
 
 	results = {}
 
-
-	firstiter = True
 	# reading line by line and performing aggregations
-	for line in input:
-		datalist = line.split(split)
-		groupingstring = "".join([datalist[int(attr)-1] for attr in attrlist])
 
-		if groupingstring in results:
+	if hasheader:
+		# This path is taken when the user passes header argument
+
+		# cols holds the header name and its index in a dict for later use
+		cols = {}
+		for idx, col in enumerate(firstline.split(split)):
+			cols[col.strip('\n')] = idx
+
+		args.append(cols)
+
+		for idx,attr in enumerate(attrlist):
+			try:
+				int(attr)
+			except:
+				attrlist[idx] = cols[attr] + 1
+
+		# aggregating the rest of the lines in file or stdin reading line by line
+		for line in input:
+			datalist = line.split(split)
+			groupingstring = "|".join([datalist[int(attr) - 1] for attr in attrlist])
+
+
+			if groupingstring in results:
 				# you have to update the results in the previous iteration
-
 				for index in range(0, len(aggattrlist)):
-					results[groupingstring][index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index], results[groupingstring][index])
+					results[groupingstring][index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index],results[groupingstring][index])
 
-		else:
+			else:
 				# you would have to reset the init value and then update it
-				print("new group ",groupingstring)
-
-				results[groupingstring] = reset_init(aggfunclist,args)
-
+				results[groupingstring] = reset_init(aggfunclist, args)
 				for index in range(0, len(aggattrlist)):
-					results[groupingstring][index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index], results[groupingstring][index])
+					results[groupingstring][index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index],results[groupingstring][index])
+
+	else:
+
+		# performing multiple aggregations on first line to ensure the results are accurate
+		datalist = firstline.split(split)
+		groupingstring = "|".join([datalist[int(attr) - 1] for attr in attrlist])
+		results[groupingstring] = reset_init(aggfunclist, args)
+
+		for index in range(0, len(aggattrlist)):
+			results[groupingstring][index] = aggregate_func(aggfunclist[index], firstline, args, aggattrlist[index],results[groupingstring][index])
+
+		# performing multiple aggregations per line and stored the results in dict
+		for line in input:
+			datalist = line.split(split)
+			groupingstring = "|".join([datalist[int(attr) - 1] for attr in attrlist])
+
+			if groupingstring in results:
+				# you have to update the results in the previous iteration
+				for index in range(0, len(aggattrlist)):
+					results[groupingstring][index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index],results[groupingstring][index])
+			else:
+				# you would have to reset the init value and then update it
+				results[groupingstring] = reset_init(aggfunclist, args)
+				for index in range(0, len(aggattrlist)):
+					results[groupingstring][index] = aggregate_func(aggfunclist[index], line, args, aggattrlist[index],results[groupingstring][index])
 
 
 
-	print(results)
+	return results
 
 
 
 
-def reset_init(aggfunclist,args,init=[]):
-
+def reset_init(aggfunclist,args):
+	'''
+	Resetting the init
+	:param aggfunclist:
+	:param args:
+	:return:
+	'''
+	init = []
 	for aggfunc in aggfunclist:
 		if aggfunc == 'sum' or aggfunc == 'count':
 			init.append(0)
@@ -259,15 +333,46 @@ def reset_init(aggfunclist,args,init=[]):
 
 
 
+def pretty_print(results,args):
+	'''
+	This function is  to print the final results to stdoutput
+	:param results:
+	:param args:
+	:return: void
+	'''
+	attributes, input, output, hasheader, split, aggfunc = args
+	aggfunclists = aggfunc.split(',')
+
+	aggattrlist = []
+	aggfunclist = []
+
+	for func in aggfunclists:
+		aggattrlist.append(re.search(r'\((.*?)\)', func).group(1))
+		aggfunclist.append(func.split('(')[0])
+
+	aggfunclists.insert(0, 'GroupBy')
+	t = PrettyTable(aggfunclists)
+	for k,v in results.items():
+		drow=[]
+		drow.append(k)
+		for idx,value in enumerate(v):
+			if aggfunclist[idx] == 'sum' or aggfunclist[idx] == 'count':
+				drow.append(value)
+			elif aggfunclist[idx] == 'min' or aggfunclist[idx] == 'max':
+				drow.append(value[0])
+			elif aggfunclist[idx] == 'avg':
+				drow.append(value[2])
+		t.add_row(drow)
+	print(t)
+
 
 def main():
 	args = user_interface()
 	args = set_input_output(args)
 	print(args)
-	sort_input(args)
-	group_by(args)
-	#group_by_hash(args)
-
+	firstline = column_offset_validation(args)
+	results = group_by_hash(args,firstline)
+	pretty_print(results,args)
 
 
 
